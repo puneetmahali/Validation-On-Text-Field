a780f569bbe81a4b70846314626b44f85d8177e0
Puneet Mahali <puneetawt@gmail.com>
2018-05-14 01:14:30 +0200

    Storyboard test

---
+++ TextFieldValidations/TextFieldValidations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextFieldValidations
+//
+//  Created by Vineet Mahali on 13/05/18.
+//  Copyright © 2018 Puneet Mahali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TextFieldValidations/TextFieldValidations/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TextFieldValidations
+//
+//  Created by Vineet Mahali on 13/05/18.
+//  Copyright © 2018 Puneet Mahali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
